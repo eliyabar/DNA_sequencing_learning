931a53954d1ab1574534e07575c5af52006cf262
rotemsd <saadorotem@gmail.com>
2017-01-13 18:34:34 +0200

    code that testing logistic regression algorithm

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
+from patsy.highlevel import dmatrices
+from sklearn import metrics
+from sklearn.cross_validation import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, ENGLISH_STOP_WORDS
 import numpy as np
 import pandas as pd
 import string
 import math
-import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-import collections, re
-from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import KMeans
+from sklearn.linear_model import LogisticRegression
 
-FILE_NAME = 'Data_tar2.csv'
 
-def read_csv_file(file = FILE_NAME):
+def read_csv_file(file=None):
     if not file:
         print("No file Name")
         return
@@ -20,9 +19,9 @@ def read_csv_file(file = FILE_NAME):
 
     df = pd.read_csv(file)
 
-    df = df.FullDescription
+    full_desc = df.FullDescription
     data_list = []
-    for line in df:
+    for line in full_desc:
         # remove punctuations
         stripped_line = ''.join(ch for ch in line if ch not in exclude)
         # lowercase line
@@ -32,199 +31,93 @@ def read_csv_file(file = FILE_NAME):
 
         data_list.append(stripped_line)
 
-    return data_list
-
-
-
-def pca(input_mat, vars=None, num_of_reduced_features=2):
-    input_mat = np.float32(input_mat)
-    input_mat = np.array(input_mat);
-
-    # subtract mean
-    avg = np.mean(input_mat, axis=0)
-    avg = np.tile(avg, (input_mat.shape[0], 1))
-    input_mat -= avg
-
-    # covariance matrix
-    cov = np.cov(input_mat.T)
-    eig_values, eig_vecs = np.linalg.eig(cov)
-
-    idx = eig_values.argsort()
-    idx = idx[::-1]
-    eig_values = eig_values[idx]
-    eig_vecs = eig_vecs[:, idx]
-
-    if vars:
-        var_exp = eig_values / np.sum(np.diag(cov))
-        var_exp = [i for i in var_exp if i >= vars]
-        eig_vecs = eig_vecs[:, :len(var_exp)]
-    else:
-        eig_vecs = eig_vecs[:, :num_of_reduced_features]
-
-    # new coordinate in new space
-    return np.dot(input_mat, eig_vecs).real
-
-
-
-
-
-def extract_features(docs):
-    return
-
-
-# def extract_features2(data):
-#     data_split = ' '.join([str(w) for doc in data for w in doc])
-#     # print data_split
-#     bag_of_words =  [collections.Counter(re.findall(r'\w+', txt)).most_common(10) for txt in data_split]
-#     return bag_of_words
-
-
-
-def bag_of_words(words):
-    bagofwords = [collections.Counter(re.findall(r'\w+', word))for word in words]
-    return  sum(bagofwords, collections.Counter())
-
+    return df.iloc[:,3:5], data_list
 
 def print_list(list):
     for line in list:
         print(line)
 
-# Metrics
-def jaccard_similarity(query, document):
-    intersection = set(query).intersection(set(document))
-    union = set(query).union(set(document))
 
-    return len(intersection) / len(union)
-
-def cosine_similarity(vector1, vector2):
-    dot_product = sum(p*q for p,q in zip(vector1, vector2))
-    magnitude = math.sqrt(sum([val**2 for val in vector1])) * math.sqrt(sum([val**2 for val in vector2]))
-    if not magnitude:
-        return 0
-    return dot_product/magnitude
-
-
-
-# kmeans clustering algorithm
-# data = set of data points
-# k = number of clusters
-# c = initial list of centroids (if provided)
-#
-def kmeans(data, k, max_itr):
-    centroids = []
-
-    centroids = randomize_centroids(data, centroids, k)
-
-    old_centroids = [[] for i in range(k)]
-
-    iterations = 0
-    while (not (has_converged(centroids, old_centroids, iterations))) or iterations <= max_itr:
-        iterations += 1
-
-        clusters = [[] for i in range(k)]
-
-        # assign data points to clusters
-        clusters = euclidean_dist(data, centroids, clusters)
-
-        # recalculate centroids
-        index = 0
-        for cluster in clusters:
-            old_centroids[index] = centroids[index]
-            centroids[index] = np.mean(cluster, axis=0).tolist()
-            index += 1
-
-
-    print("The total number of data instances is: " + str(len(data)))
-    print("The total number of iterations necessary is: " + str(iterations))
-    print("The means of each cluster are: " + str(centroids))
-    print("The clusters are as follows:")
-    for cluster in clusters:
-        print("Cluster with a size of " + str(len(cluster)) + " starts here:")
-        print(np.array(cluster).tolist())
-        print("Cluster ends here.")
-
-    return
-
-# Calculates euclidean distance between
-# a data point and all the available cluster
-# centroids.
-def euclidean_dist(data, centroids, clusters):
-    for instance in data:
-        # Find which centroid is the closest
-        # to the given data point.
-        mu_index = min([(i[0], np.linalg.norm(instance-centroids[i[0]])) \
-                            for i in enumerate(centroids)], key=lambda t:t[1])[0]
-        try:
-            clusters[mu_index].append(instance)
-        except KeyError:
-            clusters[mu_index] = [instance]
-
-    # If any cluster is empty then assign one point
-    # from data set randomly so as to not have empty
-    # clusters and 0 means.
-    for cluster in clusters:
-        if not cluster:
-            cluster.append(data[np.random.randint(0, len(data), size=1)].flatten().tolist())
-
-    return clusters
-
-
-# randomize initial centroids
-def randomize_centroids(data, centroids, k):
-    for cluster in range(0, k):
-        centroids.append(data[np.random.randint(0, len(data), size=1)].flatten().tolist())
-    return centroids
-
-
-# check if clusters have converged
-def has_converged(centroids, old_centroids, iterations):
-    MAX_ITERATIONS = 1000
-    if iterations > MAX_ITERATIONS:
-        return True
-    return old_centroids == centroids
+class LogisticReg(object):
+    pass
 
 
 if __name__ == '__main__':
     print("--In Main--")
 
-    # full_file = read_csv_file()
-    # full_file = full_file[:1000]
-    # print_list(full_file)
-    data_list = []
-    data_list.append("i am moshe play")
-    data_list.append("i am going to play")
-    data_list.append("i love intel")
+    full_file, data = read_csv_file("res/Data_tar2.csv")
+    file = full_file
 
-    vec = CountVectorizer()
-    test = vec.fit_transform(data_list).toarray()
-    print(test)
-    print(vec.get_feature_names())
-    # print(test)
+    init_vec = CountVectorizer(token_pattern='\\b\\w+\\b')
+    vec = init_vec.fit_transform(data).toarray()
 
-    # vectorizer = CountVectorizer(max_features=200)
+    # print(init_vec.get_feature_names())
+    vec = pd.DataFrame(vec)
+    vec.columns = init_vec.get_feature_names()
+    vec['Category'] = file.Category
 
-    # here we make this matrix with good scatter for EX
-    # corraletion - when x gets bigger y gets bigger
+    categories = sorted(set(vec['Category']))
+    categories_map = {k: v for v, k in enumerate(categories)}
+    # print (categories_map)
+    vec['Category'] =vec['Category'].map(categories_map)
+    # print(vec)
 
+    format = 'Category ~ '
+    for col in init_vec.get_feature_names():
+        format += "%s + " % col
+    format = format[:-3]
+    # print(format)
+    # y, X = dmatrices(format, vec, return_type="dataframe")
 
-    # X = vectorizer.fit_transform(full_file).toarray()
-    # np.set_printoptions(threshold=np.nan)
-    # print(X[:, 0])
-    # print(X)
-    # print(X.shape)
-    # plt.scatter(X[:, 0:22])
-    # plt.axis('auto')
-    # plt.show()
-    # print(bag_of_words(full_file))
-    # print(vectorizer.get_feature_names())
+    X = vec.iloc[:,:-1]
+    X.insert(0,'Intercept',1)
+    X = X.astype(float)
 
-    # p1 = pca(X,None, num_of_reduced_features=2)
-    # print(kmeans.labels_)
-    # pca = PCA(n_components=2)  # project from 64 to 2 dimensions
-    # projected = pca.fit_transform(X)
-
-    print("\n\n\n")
-    # print(projected)
+    y = np.ravel(vec['Category'])
+    y = y.astype(float)
 
 
 
+
+    # X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.5, random_state=1)
+    # print(X_train)
+
+    test_size =int(len(X.index)/2)
+    # print(test_size)
+    # print(categories_map)
+
+    X_train = X.iloc[0:test_size]
+    X_test = X.iloc[test_size:]
+
+    y_train = y[0:test_size]
+    y_test = y[test_size:]
+
+
+    model = LogisticRegression()
+    model = model.fit(X_train,y_train)
+
+    predicted = model.predict(X_test)
+    print(predicted)
+
+    accuracy = metrics.accuracy_score(y_test, predicted)
+    print("Regular accuracy: %s" % accuracy)
+
+
+
+
+
+
+    pca = PCA(n_components=2)  # project from 64 to 2 dimensions
+    X_pca = pca.fit_transform(X)
+
+    X_pca_train = X_pca.iloc[0:test_size]
+    X_pca_test = X_pca.iloc[test_size:]
+
+    model_pca = LogisticRegression()
+    model_pca = model_pca.fit(X_pca_train, y_train)
+
+    predicted_pca = model_pca.predict(X_pca_test)
+    print(predicted_pca)
+
+    accuracy_pca = metrics.accuracy_score(y_test, predicted_pca)
+    print("pca accuracy: %s" % accuracy_pca)
