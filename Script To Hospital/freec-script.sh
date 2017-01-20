b8770bed30c686435a68ac07bd994105b845a628
rotemsd <saadorotem@gmail.com>
2017-01-20 15:33:37 +0200

    Added script that will collect results from hospital DB

---
+++ Script To Hospital/freec-script.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+DIR=$(dirname $0)
+FREEC_PATH=/home/rotemsd/Desktop/FinalProject/FREEC-9.7b/src/freec
+OUTPUT_PATH=/home/rotemsd/Desktop
+
+
+usage()
+{
+    echo "usage: [-f | --file <PATH TO FILE>]"
+}
+
+if [ "$1" == "" ]; then
+	usage
+    	exit 1
+fi
+
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -f | --file )           shift
+                                file=$1
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+if [[ ("$file" == "")]]; then
+	usage
+    	exit 1
+fi
+
+array=() # Create array
+while IFS= read -r line # Read a line
+do
+	array+=("$line") # Append line to the array
+done < "$file"
+
+RESULTS_PATH=$OUTPUT_PATH/outputResults/
+
+if [ ! -d $RESULTS_PATH ]
+	then mkdir $RESULTS_PATH
+fi
+
+# printf '%s\n' "${array[@]}"
+
+for ((i = 0; i < ${#array[@]}; ++i)); do
+
+	path=${array[$i]}
+	fileName=$(basename $path)
+	fileName="${fileName%.*}"
+	mainfolder="id_${i}_${fileName}"
+
+	mkdir $RESULTS_PATH/$mainfolder
+
+	#printf '%s\n' "${path}"
+	
+	sex="XY"
+
+	sed -i "s@mateFile =.*@mateFile = ${path}@" config.txt
+
+	sed -i "s@sex =.*@sex = ${sex}@" config.txt
+
+
+	outputDir=$RESULTS_PATH/$mainfolder/window500/
+
+	mkdir $outputDir
+	
+	sed -i "s/window =.*/window = 500/" config.txt
+
+	sed -i "s@outputDir =.*@outputDir =${outputDir}@" config.txt
+
+	$FREEC_PATH -conf ./config.txt
+
+
+	outputDir=$RESULTS_PATH/$mainfolder/window10000/
+
+	mkdir $outputDir
+	
+	sed -i "s/window =.*/window = 10000/" config.txt
+
+	sed -i "s@outputDir =.*@outputDir =${outputDir}@" config.txt
+
+	$FREEC_PATH -conf ./config.txt
+
+
+	outputDir=$RESULTS_PATH/$mainfolder/window50000/
+
+	mkdir $outputDir
+	
+	sed -i "s/window =.*/window = 50000/" config.txt
+
+	sed -i "s@outputDir =.*@outputDir =${outputDir}@" config.txt
+
+	$FREEC_PATH -conf ./config.txt
+	
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
