0f1548e3a8c087e04d5bc33f839536a8e60a4bf9
rotemsd <saadorotem@gmail.com>
2017-04-24 13:49:43 +0300

    update script for generating Data only for window size - 100

--- Script To Hospital/freec-script.sh
+++ Script To Hospital/freec-script.sh
@@ -65,97 +65,18 @@ for ((i = 0; i < ${#array[@]}; ++i)); do
 
 	sed -i "s@sex =.*@sex = ${sex}@" config.txt
 
-    # configuration for window size 500
-	outputDir=$RESULTS_PATH/$mainfolder/window500/
+    # configuration for window size 100
+	outputDir=$RESULTS_PATH/$mainfolder/window100/
 
 	mkdir $outputDir
 	
-	sed -i "s/window =.*/window = 500/" config.txt
+	sed -i "s/window =.*/window = 100/" config.txt
 
 	sed -i "s@outputDir =.*@outputDir =${outputDir}@" config.txt
 
-	cp config.txt $outputDir/config_w500.txt
+	cp config.txt $outputDir/config_w100.txt
 
-	$FREEC_PATH -conf $outputDir/config_w500.txt
-
-    # configuration for window size 10000
-	outputDir=$RESULTS_PATH/$mainfolder/window10000/
-
-	mkdir $outputDir
-	
-	sed -i "s/window =.*/window = 10000/" config.txt
-
-	sed -i "s@outputDir =.*@outputDir =${outputDir}@" config.txt
-
-	cp config.txt $outputDir/config_w10000.txt
-
-	$FREEC_PATH -conf $outputDir/config_w10000.txt
-
-    # configuration for window size 50000
-	outputDir=$RESULTS_PATH/$mainfolder/window50000/
-
-	mkdir $outputDir
-	
-	sed -i "s/window =.*/window = 50000/" config.txt
-
-	sed -i "s@outputDir =.*@outputDir =${outputDir}@" config.txt
-
-	cp config.txt $outputDir/config_w50000.txt
-
-	$FREEC_PATH -conf $outputDir/config_w50000.txt
-
-    # configuration for window size 50
-	outputDir=$RESULTS_PATH/$mainfolder/window50/
-
-	mkdir $outputDir
-	
-	sed -i "s/window =.*/window = 50/" config.txt
-
-	sed -i "s@outputDir =.*@outputDir =${outputDir}@" config.txt
-
-	cp config.txt $outputDir/config_w50.txt
-
-	$FREEC_PATH -conf $outputDir/config_w50.txt
-
-    # configuration for window size 25
-	outputDir=$RESULTS_PATH/$mainfolder/window25/
-
-	mkdir $outputDir
-	
-	sed -i "s/window =.*/window = 25/" config.txt
-
-	sed -i "s@outputDir =.*@outputDir =${outputDir}@" config.txt
-
-	cp config.txt $outputDir/config_w25.txt
-
-	$FREEC_PATH -conf $outputDir/config_w25.txt
-
-    # configuration for window size 10
-	outputDir=$RESULTS_PATH/$mainfolder/window10/
-
-	mkdir $outputDir
-	
-	sed -i "s/window =.*/window = 10/" config.txt
-
-	sed -i "s@outputDir =.*@outputDir =${outputDir}@" config.txt
-
-	cp config.txt $outputDir/config_w10.txt
-
-	$FREEC_PATH -conf $outputDir/config_w10.txt
-
-    # configuration for window size 2
-	outputDir=$RESULTS_PATH/$mainfolder/window2/
-
-	mkdir $outputDir
-	
-	sed -i "s/window =.*/window = 2/" config.txt
-
-	sed -i "s@outputDir =.*@outputDir =${outputDir}@" config.txt
-
-	cp config.txt $outputDir/config_w2.txt
-
-	$FREEC_PATH -conf $outputDir/config_w2.txt
-	
+	$FREEC_PATH -conf $outputDir/config_w100.txt	
 done
 
 # end of script
