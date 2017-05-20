2c3681a24466c2e0d7688fbe0930af1bc9a4cf07
elya-PC\elya <flashxyz@gmail.com>
2017-05-21 01:21:02 +0300

    Added results. Added function that will gather all outputs in one excel file, without graphs.

--- VectorToHistograms/vectorToHistograms.py
+++ VectorToHistograms/vectorToHistograms.py
@@ -16,7 +16,6 @@ def open_file(path):
         array = [0] * n
         for i, line in enumerate(f):
             index = int(float(line.split()[2]) / (max_hits / (math.pow(10, math.log10(n)))))
-            # print("line: " + str(i) + "  " + str(index))
             if index >= n:
                 array[n - 1] += 1
             else:
@@ -61,7 +60,7 @@ def create_excel_file_with_graph(data, index_names, file_name):
     df.to_excel(writer, sheet_name='Sheet1')
 
     # Get the xlsxwriter workbook and worksheet objects.
-    workbook  = writer.book
+    workbook = writer.book
     worksheet = writer.sheets['Sheet1']
 
     # Create a chart object.
@@ -83,25 +82,42 @@ def create_excel_file_with_graph(data, index_names, file_name):
     writer.save()
 
 
+def all_to_one_excel_file(paths, indexes, file_name):
+    print("Starting One excel file")
+    writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
+
+    df = pd.DataFrame()
+    df['index'] = indexes
+
+    for path in paths:
+        print("working on: ", path)
+        normalized_array = normalize(open_file(path))
+        df[path.split('\\')[-1]] = normalized_array
+
+    df.to_excel(writer, sheet_name='Sheet1', index=False)
+    writer.save()
+    print("File ", file_name, " Saved")
 
 
 if __name__ == '__main__':
-    ds = dirScanner("\FREEC_OUT2", "cpn")
-    normal_paths, toumor_paths = ds.get_paths()
 
     index_names = create_index_names()
-    index = 0
-    for path in normal_paths:
-        print("Working on ", path)
-        normalized_array = normalize(open_file(path))
-        create_excel_file_with_graph(normalized_array, index_names, "output_normal" + str(index) + ".xlsx")
-        index += 1
 
-    index = 0
-    for path in toumor_paths:
-        print("Working on ", path)
-        normalized_array = normalize(open_file(path))
-        create_excel_file_with_graph(normalized_array, index_names, "output_toumor" + str(index) + ".xlsx")
-        index += 1
+    ds = dirScanner("\\\\192.168.1.16\Public\FREEC_OUT2", "cpn")
+    normal_paths, toumor_paths = ds.get_paths()
 
+    #  Option 1 - if we want to print each  cpn to excel file with graph.
 
+    # for path in normal_paths:
+    #     print("Working on ", path)
+    #     normalized_array = normalize(open_file(path))
+    #     create_excel_file_with_graph(normalized_array, index_names, path.split('\\')[-1] + ".xlsx")
+    #
+    # for path in toumor_paths:
+    #     print("Working on ", path)
+    #     normalized_array = normalize(open_file(path))
+    #     create_excel_file_with_graph(normalized_array, index_names, path.split('\\')[-1] + ".xlsx")
+
+    # Option 2 - all cpn files are in one excel file without graph
+    all_to_one_excel_file(normal_paths, index_names, "output_of_normal.xlsx")
+    all_to_one_excel_file(toumor_paths, index_names, "output_of_toumor.xlsx")
