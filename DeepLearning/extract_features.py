d0efbbf9929a7b9304a3613b7984a06f68e97669
rotemsd <saadorotem@gmail.com>
2017-06-12 23:10:50 +0300

    add console menu + give weight to samples

--- DeepLearning/extract_features.py
+++ DeepLearning/extract_features.py
@@ -8,15 +8,18 @@ class ExtractFeatures:
         self._x_data = x_data
         self._y_data = y_data
         self._num_of_features = num_of_features
-        self.extract_features()
         self._all_features = list(self._x_data)
         self._selected_features = []
+        self._sample_weigth = [10, 10, 10, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                               1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        self.extract_features()
         print("Extract features completed")
 
     def extract_features(self):
         i = 0
-        logistic_object = LogisticRegression()
+        logistic_object = LogisticRegression(solver='lbfgs')
         while i < self._num_of_features:
+            print("selecting feature number: ", i + 1)
             tested_features = list(set(self._all_features) - set(self._selected_features))
             max_score = 0
             selected_feature = None
@@ -25,14 +28,14 @@ class ExtractFeatures:
                 temp_selected_features.append(feature)
                 temp_x_df = self._x_data[temp_selected_features].copy()
                 # print(temp_x_df)
-                logistic_object.fit(temp_x_df, self._y_data)
+                logistic_object.fit(temp_x_df, self._y_data, sample_weight=self._sample_weigth)
                 score = logistic_object.score(temp_x_df, self._y_data)
-                # print("score: " + str(score))
                 if score > max_score:
                     max_score = score
                     selected_feature = feature
-                if max_score == 1.0:
-                    break
+                    print("Selected feature: " + selected_feature + ", score: " + str(score))
+                if score >= max_score:
+                    print("Feature: " + feature + ", score: " + str(score))
 
             self._selected_features.append(selected_feature)
             # print("finish: ", self._selected_features)
--- DeepLearning/main.py
+++ DeepLearning/main.py
@@ -3,9 +3,9 @@ from extract_features import ExtractFeatures
 from sklearn.cluster import KMeans
 from prepare_data import PrepareData
 import numpy as np
-import os
 import pandas as pd
 import timeit
+import sys, os
 
 DATA_FILES_PATH = "../DataSavedInCSV"
 
@@ -15,7 +15,7 @@ def save_all_data_to_CSVs():
     data = PrepareData("D:\FinalProject\FREEC_OUT2", union_window_number=500)
     x_data, y_data = data.get_matrix()
     headers = data.get_columns_name_list()
-    np.savetxt(os.path.join(DATA_FILES_PATH, "x_data_window_50000.csv"), x_data, delimiter=",", fmt='%10.5f', header=headers, comments="")
+    np.savetxt(os.path.join(DATA_FILES_PATH, "x_data_window_1000.csv"), x_data, delimiter=",", fmt='%10.5f', header=headers, comments="")
     np.savetxt(os.path.join(DATA_FILES_PATH, "y_data.csv"), y_data, delimiter=",", fmt='%10.5f', header="y", comments="")
 
     # data = PrepareData("D:\FinalProject\FREEC_OUT2", union_window_number=100)
@@ -38,37 +38,58 @@ def load_data_from_csv_file(file_name):
     # return np.genfromtxt(os.path.join(DATA_FILES_PATH,file_name), delimiter=',', dtype=int)
     return pd.read_csv(os.path.join(DATA_FILES_PATH,file_name), dtype=int)
 
-if __name__ == '__main__':
 
-    # # save all data to CSV files
-    # save_all_data_to_CSVs()
+# =======================
+#     MENUS FUNCTIONS
+# =======================
 
-    # init start time variable for measurement time execution
-    start_time = timeit.default_timer()
+# Main menu
+def show_menu():
+    print("Please choose the menu you want to start:")
+    print("1. extract 50 features")
+    print("2. run k-means")
+    print("3. discover the main affect features")
+    choice = input(" >>  ")
+    exec_menu(choice)
+    return
 
+
+# Execute menu
+def exec_menu(choice):
+    ch = choice.lower()
+    try:
+        menu_actions[ch]()
+    except KeyError:
+        print
+        "Invalid selection, please try again.\n"
+        show_menu()
+    return
+
+
+def menu_extract_features():
+    print("menu_extract_features")
     # get data from CSV
-    x_df = load_data_from_csv_file("x_data_window_50000_data_duplicate_normalized.csv")
+    x_df = load_data_from_csv_file("x_data_window_50000.csv")
     y_df = np.ravel(load_data_from_csv_file("y_data.csv"))
 
-    # calculate the elapsed time
-    elapsed = timeit.default_timer() - start_time
-    print("load data from csv file: %s\n" % elapsed)
+    logistic_object = LogisticRegression()
+    logistic_object.fit(x_df, y_df)
+    score = logistic_object.score(x_df, y_df)
+    print("Before extract features final score: " + str(score))
 
-    # x_df = x_df.iloc[:, :10]
-    # print(x_df)
+    extract_df = ExtractFeatures(x_df, y_df, num_of_features=50)
+    x_df_extraced_features = extract_df.get_extracted_features()
 
-    # logistic_object = LogisticRegression()
-    # logistic_object.fit(x_df, y_df)
-    # score = logistic_object.score(x_df, y_df)
-    # print("Before extract features final score: " + str(score))
-    #
-    # extract_df = ExtractFeatures(x_df, y_df, num_of_features=50)
-    # x_df_extraced_features = extract_df.get_extracted_features()
-    #
-    # logistic_object = LogisticRegression()
-    # logistic_object.fit(x_df_extraced_features, y_df)
-    # score = logistic_object.score(x_df_extraced_features, y_df)
-    # print("After extract features final score: " + str(score))
+    logistic_object = LogisticRegression()
+    logistic_object.fit(x_df_extraced_features, y_df)
+    score = logistic_object.score(x_df_extraced_features, y_df)
+    print("After extract features final score: " + str(score))
+
+
+def menu_k_means():
+    print("menu_k_means")
+    # get data from CSV
+    x_df = load_data_from_csv_file("x_data_window_50000.csv")
 
     kmeans = KMeans(n_clusters=2)
     kmeans.fit(x_df)
@@ -77,4 +98,45 @@ if __name__ == '__main__':
 
     print(centroids)
     print(labels)
+
+
+def menu_discover_affect_features():
+    print("menu_discover_affect_features")
+    # get data from CSV
+    x_df = load_data_from_csv_file("x_data_window_50000.csv")
+    y_df = np.ravel(load_data_from_csv_file("y_data.csv"))
+
+    extract_df = ExtractFeatures(x_df, y_df, num_of_features=1)
+    extract_df.get_extracted_features()
+
+
+# Exit program
+def exit():
+    sys.exit()
+
+
+# =======================
+#    MENUS DEFINITIONS
+# =======================
+
+# Menu definition
+menu_actions = {
+    '1': menu_extract_features,
+    '2': menu_k_means,
+    '3': menu_discover_affect_features,
+    '9': exit,
+}
+
+# =======================
+#      MAIN PROGRAM
+# =======================
+
+
+if __name__ == '__main__':
+    # # save all data to CSV files
+    # save_all_data_to_CSVs()
+
+    # Launch main menu
+    show_menu()
+
 
